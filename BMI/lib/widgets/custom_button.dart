b09e02031865cc1a7460765d2326da9492050402
ahmed khaled <ahmedelads789@gmail.com>
2024-04-25 01:18:57 +0200

    TeknoSoft

---
+++ BMI/lib/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          fixedSize:const Size(double.infinity, 55),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blue,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        child: (
+            const Row (
+              mainAxisAlignment:MainAxisAlignment.center,
+              children: [
+                Text("TRY AGAIN",style: TextStyle(fontSize: 20),),
+                Icon(Icons.refresh_outlined),
+              ],
+            )));
+  }
+}
